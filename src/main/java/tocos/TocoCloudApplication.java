f274d6e797e647e8fbb1382366f7aed89cfd75d2
Zavik <zav021104@gmail.com>
2024-11-14 20:21:20 +0700

    chore: set up initial project structure with Spring Boot

---
+++ src/main/java/tocos/TocoCloudApplication.java
@@ -0,0 +1,13 @@
+package tocos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocoCloudApplication.class, args);
+	}
+
+}
